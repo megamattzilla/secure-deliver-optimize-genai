edda46e4a38c2fd06c50bac4155ab368f0ca19b8
fochan <foobang.chan@f5.com>
2025-01-07 08:09:55 +1100

    updated .gitignore and remove tracked foler

--- build-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-./containthedocs-build.sh
-
-docker build --platform linux/amd64 --no-cache -t reg.edgecnf.com/apps/genai-lab-guide:v0.1 -f Dockerfile .
-
-docker push reg.edgecnf.com/apps/genai-lab-guide:v0.1
-
